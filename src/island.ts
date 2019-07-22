67e5c98e1133e31f3246edaf92df69a932d67e0c
PJ81 <paulo.jorente@web.de>
2019-07-22 14:00:56 +0200

    place a guess

--- src/island.ts
+++ src/island.ts
@@ -6,33 +6,36 @@ export default class Island {
   size: number;
   posX: number;
   posY: number;
-  conCount: number;
   selected: boolean;
   connections: Connection[];
+  guesses: Connection[];
 
   hasFreeConnection: (dir: number) => boolean;
   hasPoint: (x: number, y: number) => boolean;
   setConnection: (dir: number, i: Island) => Connection;
+  setGuess: (t: any) => number;
 
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
-    this.selected = false;
     this.size = 21;
-    this.conCount = 0;
+    this.selected = false;
     this.posX = this.size + this.x * this.size + this.size * this.x;
     this.posY = this.size + this.y * this.size + this.size * this.y;
     this.connections = [null, null, null, null];
+    this.guesses = [null, null, null, null];
 
     this.hasFreeConnection = (dir: number): boolean => !this.connections[dir];
     this.setConnection = (dir: number, i: Island) => this.connections[dir] = new Connection(dir, i);
     this.hasPoint = (x: number, y: number): boolean => !(x < this.posX || x > this.posX + this.size || y < this.posY || y > this.posY + this.size);
+    this.setGuess = (t) => this.guesses[t].count++;
   }
 
   draw(ctx: CanvasRenderingContext2D) {
     ctx.rect(this.posX, this.posY, this.size, this.size);
-    const cc = this.connections.reduce((acc, con) => acc + (con ? con.count : 0), 0);
-    if (this.conCount === cc) {
+    const cc = this.connections.reduce((acc, con) => acc + (con ? con.count : 0), 0),
+      gc = this.guesses.reduce((acc, con) => acc + (con ? con.count : 0), 0);
+    if (gc === cc) {
       ctx.fillStyle = "rgba(0, 255, 0)";
       ctx.fillRect(this.posX, this.posY, this.size, this.size);
       ctx.fillStyle = "#000";
